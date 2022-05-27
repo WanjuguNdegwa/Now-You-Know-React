e53143566495a315e3738ba49fa2146e0218fdf9
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-05-27 06:50:26 +0300

    Add bookmarking functionality

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -1,10 +1,28 @@
-import { FaTwitter } from 'react-icons/fa'
+import { FaTwitter, FaRegBookmark } from 'react-icons/fa'
+import { doc, collection, setDoc } from 'firebase/firestore'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+import { db, auth } from '../firebase'
 
 const NewsArticle = ({article}) => {
 
+  const [user] = useAuthState(auth)
+
+  const saveArticle = async () => {
+    try {
+      if (user) { 
+        const articleRef = doc(db, "users", user.uid, "articles", article.url.replace(/\//g,"-"))
+        await setDoc(articleRef, article)
+        console.log("Document written with ID: ", articleRef.id)
+      }
+    } catch (e) {
+      console.error("Error adding document: ", e)
+    }
+  }
+
   const stripHtml = (html) => {
-    let doc = new DOMParser().parseFromString(html, 'text/html');
-    return doc.body.textContent || "";
+    let doc = new DOMParser().parseFromString(html, 'text/html')
+    return doc.body.textContent || ""
   }
   return (
     <div className="card my-3 news-article">
@@ -22,7 +40,7 @@ const NewsArticle = ({article}) => {
               <a href={`https://twitter.com/intent/tweet?text=${article.title}&url=${article.url}&hashtags=NowYouKnow`}>
                 <FaTwitter />
               </a>
-              <span className="like"><i className="fa-solid fa-heart"></i></span>
+              <span className="like" onClick={saveArticle}><FaRegBookmark /></span>
           </div>
       </div>
   </div>
