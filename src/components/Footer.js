11ba56e56d76dc7b804b9c17c8765d00bd3dd35c
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-05-27 06:51:32 +0300

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,8 @@
-import { Link, useLocation } from 'react-router-dom'
-
 const Footer = () => {
-  const {pathname} = useLocation();
 
   return (
-    <footer>
-      <p>Copyright &copy; 2022</p>
-      {pathname !== '/about' && <Link to="/about">About</Link>}
+    <footer className='footer'>
+      <p>Copyright &copy; Ann Ndegwa 2022</p>
     </footer>
   )
 }
