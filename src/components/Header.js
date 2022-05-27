fb083cbfebded2f11914b03d646eb10475fcd79c
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-05-27 06:51:02 +0300

    Change header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
-import WeatherWidget from "./WeatherWidget"
+import WeatherWidget from "./WeatherWidget";
+import SignIn from "./SignIn";
+import "./Header.css";
 
-const Header = ({weather, location}) => {
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import SignOut from './SignOut';
+
+const Header = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
-    <header className="d-flex justify-content-center align-items-end">
-        <h1>
-            Now You Know
-        </h1>
-        <WeatherWidget />
+    <header className="header">
+      <WeatherWidget />
+      <h1>Now You Know</h1>
+      {user ? <div> {user.email} <SignOut /></div> : <SignIn isDisabled={loading} />}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
